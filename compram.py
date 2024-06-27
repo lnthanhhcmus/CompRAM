7706035686d015357ee089691f1a27f502941955
Thanh Le <lnthanh@fit.hcmus.edu.vn>
2024-06-27 08:47:31 +0700

    Add files via upload

---
+++ compram.py
@@ -0,0 +1,9 @@
+from helper import *
+from message_passing import MessagePassing
+from special_spmm_func import SpecialSpmmFunc
+import torch.nn as nn
+
+class CompRAM(MessagePassing):
+    """
+    To ensure the authenticity and reproducibility of our findings, the source code related to this research will be made publicly available following the successful advancement beyond the initial review phase.
+    """
